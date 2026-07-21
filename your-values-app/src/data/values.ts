6b8e73525681f85f5f68133e012039fee9ff80d9
Oscar Monteverde <108705978+Oscar-Mont@users.noreply.github.com>
2026-07-21 23:48:11 +0000

    Add values data

---
+++ your-values-app/src/data/values.ts
@@ -0,0 +1,38 @@
+
+export type Value = {
+    id: number
+    name: string
+}
+
+export const values: Value[] = [
+    { id: 1, name: "Adventure" },
+    { id: 2, name: "Authenticity" },
+    { id: 3, name: "Balance" },
+    { id: 4, name: "Compassion" },
+    { id: 5, name: "Connection" },
+    { id: 6, name: "Courage" },
+    { id: 7, name: "Creativity" },
+    { id: 8, name: "Curiosity" },
+    { id: 9, name: "Discipline" },
+    { id: 10, name: "Family" },
+    { id: 11, name: "Freedom" },
+    { id: 12, name: "Friendship" },
+    { id: 13, name: "Fun" },
+    { id: 14, name: "Gratitude" },
+    { id: 15, name: "Growth" },
+    { id: 16, name: "Honesty" },
+    { id: 17, name: "Independence" },
+    { id: 18, name: "Justice" },
+    { id: 19, name: "Kindness" },
+    { id: 20, name: "Knowledge" },
+    { id: 21, name: "Loyalty" },
+    { id: 22, name: "Meaning" },
+    { id: 23, name: "Nature" },
+    { id: 24, name: "Patience" },
+    { id: 25, name: "Purpose" },
+    { id: 26, name: "Security" },
+    { id: 27, name: "Self-respect" },
+    { id: 28, name: "Spirituality" },
+    { id: 29, name: "Success" },
+    { id: 30, name: "Wisdom" },
+]
