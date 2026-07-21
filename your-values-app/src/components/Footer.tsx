ac36ddb63b63e742e5a5494dd3edaa8431a72996
Oscar Monteverde <108705978+Oscar-Mont@users.noreply.github.com>
2026-07-21 23:30:35 +0000

    Create Footer Component

---
+++ your-values-app/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+
+export default function Footer() {
+    return (
+        <div className="text-violet-50 flex flex-col text-center font-medium mb-5">
+            <p className="m-1">Made with ❤️ by Oscar Monteverde</p>
+            <p>Disclaimer: This is not an official psychological test. This is a simple game to help people explore what is most important to them.</p>
+        </div>
+    )
+}
--- your-values-app/src/pages/HomePage.tsx
+++ your-values-app/src/pages/HomePage.tsx
@@ -1,17 +1,22 @@
 import Button from "../components/Button"
 import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 export default function HomePage() {
     return (
-        <div>
+        <div className="flex flex-col min-h-screen">
             <Header />
-            <div className="flex flex-col justify-center items-center m-4">
+
+
+            <main className="flex flex-col  flex-1 justify-center items-center m-4">
                 <h1 className="text-6xl text-white text-center p-2">What are ?</h1>
                 <h1 className="text-6xl text-white text-center p-2">YOUR</h1>
                 <h1 className="text-6xl text-white text-center p-2">Values?</h1>
                 <h3 className="text-3xl text-white text-center p-2 mt-7 mb-7">A quick and simple game to help you find your top-5 values</h3>
                 <Button variant="start">Let's Do It!</Button>
-            </div>
+
+            </main>
+            <Footer />
         </div>
     )
 }
