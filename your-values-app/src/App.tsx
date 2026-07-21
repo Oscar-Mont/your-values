f6ecb2cc782665d4d5d6c0145e74d06773add715
Oscar Monteverde <108705978+Oscar-Mont@users.noreply.github.com>
2026-07-21 23:47:47 +0000

    Create Values Test Page

--- your-values-app/src/App.tsx
+++ your-values-app/src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router"
 import HomePage from "./pages/HomePage"
+import ValuesTestPage from "./pages/ValuesTestPage"
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
+      <Route path="/values-test" element={<ValuesTestPage />}></Route>
     </Routes>
   )
 }
---
+++ your-values-app/src/pages/ValuesTestPage.tsx
@@ -0,0 +1,9 @@
+import Header from "../components/Header";
+
+export default function ValuesTestPage() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
