268ed21878510f891d24d6c3ba3d81abed7cee96
Oscar Monteverde <108705978+Oscar-Mont@users.noreply.github.com>
2026-07-23 22:13:19 +0000

    Blank slate on results page

--- your-values-app/src/pages/ValuesTestPage.tsx
+++ your-values-app/src/pages/ValuesTestPage.tsx
@@ -7,61 +7,8 @@ import { values } from "../data/values";
 import { useEffect, useState } from "react";
 import { Link } from "react-router";
 
-
-const firstRemainingValues: Value[] = values.slice(10)
-const firstRoundValues = values.slice(0, 10)
-
 export default function ValuesTestPage() {
-    const [remainingValues, setRemainingValues] = useState<Value[]>(firstRemainingValues)
-    const [roundValues, setRoundValues] = useState<Value[]>(firstRoundValues)
-    const [selectedValues, setSelectedValues] = useState<Value[]>([])
 
-    function getRoundValues() {
-        if (remainingValues.length > 5) {
-            const valuesToMove = remainingValues.slice(0, 10)
-            const keptValues = remainingValues.slice(10)
-            setRemainingValues(keptValues)
-            setRoundValues(valuesToMove)
-        }
-        if (remainingValues.length === 5) {
-            console.log("Game over")
-        }
-    }
-
-
-    function handleValueToggle(value: Value) {
-        setSelectedValues(prev => {
-            const exists = prev.some(obj => obj.id === value.id)
-            if (exists) {
-                return prev.filter(obj => obj.id !== value.id)
-            }
-            if (prev.length === 5) {
-                alert("You can Only Select 5 Values per Round")
-                return prev
-            }
-            return [...prev, value]
-        })
-    }
-
-    function handleContinue() {
-        console.log("selected:", selectedValues)
-        console.log("remaining before update:", remainingValues)
-        //check if 5 are selected
-        if (selectedValues.length === 5) {
-            setRemainingValues(prev => [...prev, ...selectedValues])
-            setSelectedValues([])
-            setRoundValues([])
-            return
-        }
-        if (selectedValues.length < 5) {
-            alert("You need to select 5 values to continue")
-            return
-        }
-    }
-
-    useEffect(() => {
-        getRoundValues()
-    }, [remainingValues])
 
     return (
         <div className="flex flex-col flex-1 min-h-screen">
